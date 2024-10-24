6834ee1ab11499a6af86c13f32d0b9b621ea34d1
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-10-25 04:00:11 +0530

    feat(ui): add background image and H2 typography component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import QuickLinkCard from "@/components/quick-link-card";
 import {QUICK_LINKS} from "@/assets/data";
+import {H2} from "@/components/ui/typography";
 
 export default function Home() {
   return (
-    <main>
-      <div className="mx-auto max-w-4xl">
-        <h2>Quick Links</h2>
-        <div className="flex flex-col gap-[10px]">
+    <main className="min-h-screen bg-[url(/bg-vector.svg)] bg-cover bg-center bg-no-repeat p-[60px]">
+      <div className="">
+        <H2>Quick Links</H2>
+        <div className="mt-3 flex flex-col gap-[10px]">
           {QUICK_LINKS.map((link) => (
             <QuickLinkCard
               key={link?.id}
