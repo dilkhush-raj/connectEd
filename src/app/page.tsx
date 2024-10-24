57b92c169ac277305eebcd4b5f96492412e1fb74
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-10-25 03:23:42 +0530

    feat: add Card and QuickLinkCard components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import QuickLinkCard from "@/components/quick-link-card";
+import {QUICK_LINKS} from "@/assets/data";
+
 export default function Home() {
   return (
     <main>
-      <h1>ConnectEd</h1>
+      <div className="mx-auto max-w-4xl">
+        <h2>Quick Links</h2>
+        <div className="flex flex-col gap-[10px]">
+          {QUICK_LINKS.map((link) => (
+            <QuickLinkCard
+              key={link?.id}
+              icon={link?.icon}
+              title={link?.title || ""}
+              description={link?.description || ""}
+              variant={link?.variant}
+              className="w-[346px]"
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
